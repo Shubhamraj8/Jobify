3909347da284f04c6786758734cbf6b86d844025
Shubham Raj <shubhamrajrathaur@gmail.com>
2026-02-04 16:11:09 +0530

    cloudinary setup

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,7 +1,9 @@
+import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
 import bcrypt from 'bcrypt';
+import axios from "axios";
 
 export const registerUser = TryCatch(async (req, res, next) => {
   const { name, email, password, phoneNumber, role, bio } = req.body;
@@ -31,11 +33,22 @@ if (role === "recruiter") {
   registeredUser = user;
 
 } else if (role === "jobseeker") {
-  const file = req.file
+  const file = req.file;
+
+  if(!file){
+    throw new ErrorHandler(400, "resume file is required for jobseeker");
+  }
+
+  const fileBuffer = getBuffer(file)
+  if(!fileBuffer || !fileBuffer.content){
+    throw new ErrorHandler(500, "Failed to generate buffer");
+  }
+  const {data} = await axios.post(`${process.env.UPLOAD_SERVICE}/api/utils/upload`, {buffer: fileBuffer.content});
+
   const [user] =
-    await sql`INSERT INTO users (name, email, password, phone_number, role) VALUES
-      (${name}, ${email}, ${hashPassword}, ${phoneNumber}, ${role}) RETURNING
-      user_id, name, email, phone_number, role, created_at`;
+    await sql`INSERT INTO users (name, email, password, phone_number, role, bio, resume, resume_public_id) VALUES
+      (${name}, ${email}, ${hashPassword}, ${phoneNumber}, ${role}, ${bio}, ${data.url}, ${data.public_id}) RETURNING
+      user_id, name, email, phone_number, role, bio, resume, created_at`;
 }
 
   res.json(email);
