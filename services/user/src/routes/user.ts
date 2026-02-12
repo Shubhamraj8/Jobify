5e56e21d046185dab32927cef8a5735d9938dbea
Shubham Raj <shubhamrajrathaur@gmail.com>
2026-02-12 23:54:16 +0530

    user service : resume update, skills update

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import { isAuth } from "../middlewares/auth.js";
-import { getUserProfile, myProfile, updateUserProfile } from "../controllers/user.js";
+import { addSkillToUser, deleteSkillFromUser, getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
+import uploadFile from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
+router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
+router.put("/update/resume", isAuth, uploadFile, updateResume);
+router.post("/update/skills", isAuth, isAuth, addSkillToUser);
+router.delete("/update/skills", isAuth, deleteSkillFromUser);
 
 export default router;
